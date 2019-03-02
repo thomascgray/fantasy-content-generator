0326791ffa13161d8181743afdde30635ba54939
Unknown <tom.c.gray@gmail.com>
2019-03-02 13:46:56 +0000

    finishing current magic items

--- examples/magic_items.js
+++ examples/magic_items.js
@@ -1,4 +1,4 @@
 const MagicItems = require('../src/magic_items')
 
 console.log('****MAGIC ITEMS - WEAPONS****');
-console.log(MagicItems.generate({ type: 'weapon', schoolOfMagic: 'necromancy' }));
+console.log(MagicItems.generate({ type: 'weapon' }));
--- src/magic_items/index.js
+++ src/magic_items/index.js
@@ -4,19 +4,23 @@ const Names = require('../names');
 
 const _type = () => Utils.pick(MagicItemData.types);
 const _powerLevel = () => Utils.pick(MagicItemData.power_levels);
-const _schoolOfMagic = () => Utils.pick(MagicItemData.schools_of_magic);
+const _schoolOfMagic = type => {
+    if (type === 'weapon') {
+        return Utils.pick(Object.keys(MagicItemData.school_weapon_effects_per_level));
+    }
+    return Utils.pick(MagicItemData.schools_of_magic);
+}
 
 const _effects = (schoolOfMagic, powerLevel) => {
     const effectsCount = Utils.parseTemplate(MagicItemData.item_effects_per_level[powerLevel])
-    const schoolLevelMapKey = `${schoolOfMagic}:${powerLevel}`;
-    return Utils.pick(MagicItemData.school_weapon_effects_per_level[schoolLevelMapKey], effectsCount, true)
+    return Utils.pick(MagicItemData.school_weapon_effects_per_level[schoolOfMagic][powerLevel], effectsCount, true)
         .map(Utils.parseTemplate)
 }
 
 const generate = props => {
     const type = (props.type) ? props.type : _type();
     const powerLevel = (props.powerLevel) ? props.powerLevel : _powerLevel();
-    const schoolOfMagic = (props.schoolOfMagic) ? props.schoolOfMagic : _schoolOfMagic();
+    const schoolOfMagic = (props.schoolOfMagic) ? props.schoolOfMagic : _schoolOfMagic(type);
     const effects = (props.effects) ? props.effects : _effects(schoolOfMagic, powerLevel);
     const subtype = Utils.pick(MagicItemData.subtypes[type]);
 
