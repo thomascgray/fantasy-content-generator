f7b424459c9e009169280f86d53f9c110209bfc5
Unknown <tom.c.gray@gmail.com>
2019-03-12 20:44:40 +0000

    fix merge conflictrs

--- src/utils/index.js
+++ src/utils/index.js
@@ -132,6 +132,19 @@ const formatRace = race => {
     }
 }
 
+const generateUUID = () => { // Public Domain/MIT
+    var d = new Date().getTime();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
+
+
 const resetSeed = () => {
     Utils_rand_lastSeed = null;
     Utils_rand_lastSeededRandomFunc = null;
@@ -144,5 +157,6 @@ module.exports = {
     forCount,
     titleCase,
     formatRace,
+    generateUUID,
     resetSeed
 }
