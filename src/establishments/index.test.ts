e133929498600cd13bb9f881bcf61fbe5e839a85
Unknown <tom.c.gray@gmail.com>
2020-03-07 00:53:37 +0000

    remove more console logs

--- src/establishments/index.test.ts
+++ src/establishments/index.test.ts
@@ -3,10 +3,8 @@ import * as Utils from "../utils";
 
 describe("Establishments", () => {
   test("generate an establishment", () => {
-    Utils.forCount(1, () => {
+    Utils.forCount(50, () => {
       const establishment = Establishments.generate({ type: "inn" });
-
-      console.log("establishment", JSON.stringify(establishment, null, 2));
       expect(typeof establishment).toEqual("object");
       expect(JSON.stringify(establishment)).not.toBe(
         expect.stringContaining("undefined")
